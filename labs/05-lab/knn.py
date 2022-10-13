c69f421d16e2b4f3c7475e8802692c2890dc53d0
SwordOfDamoc1es <marvemao@bu.edu>
2022-10-13 14:38:06 -0400

    done first

--- labs/05-lab/knn.py
+++ labs/05-lab/knn.py
@@ -32,3 +32,33 @@ class KNN:
 # predict the labels using KNN
 
 # use the test function to compute the error
+
+# from sklearn import datasets
+# from sklearn.decomposition import PCA
+# import matplotlib.pyplot as plt
+# import mpl_toolkits.mplot3d
+# from sklearn.neighbors import KNeighborsClassifier
+# from sklearn.model_selection import train_test_split
+# from sklearn.datasets import load_iris
+
+
+# iris = datasets.load_iris()
+# X = iris.data[:, :2]  # we only take the first two features.
+# y = iris.target
+
+# x_min, x_max = X[:, 0].min() - 0.5, X[:, 0].max() + 0.5
+# y_min, y_max = X[:, 1].min() - 0.5, X[:, 1].max() + 0.5
+
+# plt.figure(2, figsize=(8, 6))
+# plt.clf()
+
+# # Plot the training points
+# plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Set1, edgecolor="k")
+# plt.xlabel("Sepal length")
+# plt.ylabel("Sepal width")
+
+# plt.xlim(x_min, x_max)
+# plt.ylim(y_min, y_max)
+# plt.xticks(())
+# plt.yticks(())
+# plt.show()
